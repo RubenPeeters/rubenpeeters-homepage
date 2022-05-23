6c73b69a97ddbfea2bc2dda8a65fef118b069d17
Ruben Peeters <ruben@fampeeters.be>
2022-05-23 22:52:44 +0200

    Update metadata

--- pages/_app.js
+++ pages/_app.js
@@ -1,3 +1,4 @@
+import Head from 'next/head'
 import { ChakraProvider, Box, Container } from '@chakra-ui/react'
 import NavBar from '../components/navbar'
 import { AnimatePresence } from 'framer-motion'
@@ -16,6 +17,15 @@ function MyApp({ Component, pageProps, router }) {
             }
           }}
         >
+          <Head>
+            <meta
+              name="viewport"
+              content="width=device-width, initial-scale=1"
+            />
+            <meta name="description" content="Ruben Peeters homepage" />
+            <meta name="author" content="Ruben Peeters" />
+            <title>Ruben Peeters - Homepage</title>
+          </Head>
           <NavBar path={router.asPath} />
           <Container maxW="container.md" pt={20}>
             <Component {...pageProps} key={router.route} />
